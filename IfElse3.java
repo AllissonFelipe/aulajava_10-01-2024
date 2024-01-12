513992a6b71c39588b03f9d3cd9d4353e686f826
AllissonFelipe <154332420+AllissonFelipe@users.noreply.github.com>
2024-01-12 08:42:40 -0300

    Update and rename ifelse2b.java to IfElse3.java

--- IfElse3.java
+++ IfElse3.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class ifelse2b {
+public class IfElse3 {
     public static void main(String[] args) {
         String username = "ABCD";
         String password = "1234";
